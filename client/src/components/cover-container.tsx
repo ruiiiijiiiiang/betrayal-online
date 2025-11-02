b51c11c2ec1757bddbc1da6834c29b50b15a563c
Yunfan Yang <cloudy.young@outlook.com>
2025-11-01 18:40:23 -0600

    Update cover-container.tsx

--- client/src/components/cover-container.tsx
+++ client/src/components/cover-container.tsx
@@ -8,7 +8,7 @@ export const CoverContainer = ({ children }: CoverContainerProps) => {
         <div className='h-screen overflow-y-auto bg-zinc-950'>
             <div className='fixed top-0 left-0 right-0 bottom-0 bg-[url("/bg-light.jpg")] bg-repeat bg-cover bg-center pointer-events-none'></div>
             <div className='fixed top-0 left-0 right-0 bottom-0 bg-[url("/bg-light-big.jpg")] bg-repeat bg-cover bg-center  pointer-events-none'></div>
-            <div className='relative h-screen max-w-2xl mx-auto gap-8 px-6'>
+            <div className='relative max-w-2xl mx-auto gap-8 px-6'>
                 {children}
             </div>
         </div>
